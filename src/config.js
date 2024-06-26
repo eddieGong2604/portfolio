662fe5e53fe8d3bc5e6cffc6a4f4104cd780fffa
eddy <eddy.l@aihomegroup.com.au>
2024-06-27 01:08:38 +1000

    first commit

---
+++ src/config.js
@@ -0,0 +1,68 @@
+module.exports = {
+  email: 'me@eddyle.com',
+
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/eddyGong2604',
+    },
+    {
+      name: 'Instagram',
+      url: '#',
+    },
+    {
+      name: 'Twitter',
+      url: '#',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/eddyle2604',
+    },
+    {
+      name: 'Codepen',
+      url: 'https://codepen.io/',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+    blue: '#6EC1E4',
+  },
+
+  srConfig: (delay = 200, viewFactor = 0.25) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
